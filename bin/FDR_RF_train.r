059c659307e52845c8b39c6709492bd0b46c6ba0
tdelhomme <delhomme.tiffany@gmail.com>
2020-02-25 12:57:48 +0100

    remove FP found in exac (missed germline)

--- bin/FDR_RF_train.r
+++ bin/FDR_RF_train.r
@@ -108,7 +108,7 @@ while(dim(all_calls)[1] != 0) {
     # assign status
     all_mut_table$status = NA # status as NA is for variants not used in the training
     all_mut_table[which(all_nbq>=2),"status"] = "TP" # if found in at least 2 normal cells
-    all_mut_table[which(all_nbq==1),"status"] = "FP" # if found in only 1 normal cell
+    all_mut_table[which(all_nbq==1 & (exac_all == 0 | is.na(exac_all))),"status"] = "FP" # if found in only 1 normal cell
   }
   
   if( ethnicity ){
